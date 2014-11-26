599816c4185ce98edd9a1333c38b3b6b464a6a5c
Jonathan Stowe <jns+git@gellyfish.co.uk>
2014-11-26 21:21:18 +0000

    Get some tests working

--- test/runtests.js
+++ test/runtests.js
@@ -1,1 +1,10 @@
 var testrunner = require("qunit");
+
+testrunner.run({
+   code: { path: __dirname + '/testcode_no_args.js', namespace: 'ModifierNoArgs' },
+   tests: __dirname + '/tests_no_args.js'
+});
+testrunner.run({
+   code: { path: __dirname + '/testcode_args.js', namespace: 'ModifierWithArgs' },
+   tests: __dirname + '/tests_args.js'
+});
---
+++ test/tests_no_args.js
@@ -0,0 +1,32 @@
+QUnit.module("Modifier No Arguments");
+test("before modifier", function() {
+	var obj;
+   ok(obj = new ModifierNoArgs(), "new object with before modifier");
+   var ret;
+   ok(ret = obj.before_meth(), "method with modifier");
+   ok(typeof ret === 'object', "got an array ");
+	ok(ret.length == 2,"got an array with two items");
+   ok(ret[0] == 'TEST_BEFORE', "our modified thing is first");
+   ok(ret[1] == 'BEFORE', "original item is second");
+});
+test("after modifier", function() {
+	var obj;
+   ok(obj = new ModifierNoArgs(), "new object with after modifier");
+   var ret;
+   ok(ret = obj.after_meth(), "method with modifier");
+   ok(typeof ret === 'object', "got an array ");
+	ok(ret.length == 2,"got an array with two items");
+   ok(ret[1] == 'TEST_AFTER', "our modified thing is second");
+   ok(ret[0] == 'AFTER', "original item is second");
+});
+test("around modifier", function() {
+	var obj;
+   ok(obj = new ModifierNoArgs(), "new object with around modifier");
+   var ret;
+   ok(ret = obj.around_meth(), "method with modifier");
+   ok(typeof ret === 'object', "got an array ");
+	ok(ret.length == 3,"got an array with two items");
+   ok(ret[0] == 'TEST_AROUND_FIRST', "our first modified thing is second");
+   ok(ret[1] == 'AROUND', "original item is second");
+   ok(ret[2] == 'TEST_AROUND_LAST', "our second modified thing is last");
+});
